dce84862fd45efd7bffaba9f6ea3f3ddcc6fb75b
BrunoSantos14 <bruno.canti.santos@gmail.com>
2025-04-14 09:45:42 -0300

    fix dic output

--- estudo.py
+++ estudo.py
@@ -14,34 +14,6 @@ class Estudo:
         self.df = data.get_data(ano=ano, id_modulo=id_modulo, analito=analito)
 
 
-    # def run(self):
-    #     df = self.df.copy()
-    #     df = PreProcessamento(df).get_data()
-        
-    #     if not df.empty:
-    #         lista = []
-    #         erros = []
-    #         # Para cada módulo
-    #         for modulo in df.id_modulo.unique():
-    #             aux = df[df.id_modulo == modulo]
-
-    #             # Para cada analito
-    #             for analito in aux.analito.unique():
-    #                 aux2 = aux[aux.analito == analito].dropna(how='all', axis=1)
-    #                 cola = Cola(aux2.set_index(['part','id_modulo','analito','sistema']))
-    #                 try:
-    #                     df_cluster = cola.aplicar_modelo(eps = None)
-    #                 except Exception as e:
-    #                     erros.append((modulo, analito, e))
-    #                     df_cluster = cola.aplicar_modelo(eps = 0.02)
-
-    #                 lista.append(self.__merges(df_cluster))
-
-    #     else:
-    #         return None
-
-    #     return lista, pd.DataFrame(erros, columns=['id_modulo','analito','erro'])
-
     def run(self):
         df = self.df.copy()
         df = PreProcessamento(df).get_data()
@@ -73,7 +45,6 @@ class Estudo:
         
         return None
 
-    
 
     def __merges(self, df: pd.DataFrame) -> pd.DataFrame:
         # Merge parceiros
